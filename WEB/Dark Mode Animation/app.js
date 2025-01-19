bd57be4004737d3d5486b2389b35dabf18564884
odaydid002 <odaydid002@gmail.com>
2025-01-19 15:26:15 +0100

    dark mode

---
+++ WEB/Dark Mode Animation/app.js
@@ -0,0 +1,17 @@
+const container = document.getElementById('container');
+const dark = document.querySelector('.dark')
+const light = document.querySelector('.light')
+const root = document.documentElement
+
+container.addEventListener('click', function(e){
+    isDark = e.target.classList[2] == "dark";
+    root.style.setProperty('--light', isDark?"#0E1621":"white");
+    root.style.setProperty('--dark', !isDark?"#0E1621":"white");
+
+    container.style.transform = isDark?"rotateZ(-360deg)":"rotate(360deg)"
+
+    setTimeout(function(){
+        dark.style.display = isDark?"none":"inline-block"
+        light.style.display = !isDark?"none":"inline-block"
+    }, 100)
+})
